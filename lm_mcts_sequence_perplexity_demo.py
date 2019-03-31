db3d467e53733b4d1b6ecd26fad8a9225976cd41
Luis Antunes <lantunes@gmail.com>
2019-03-30 22:04:55 -0400

    adding demo

---
+++ lm_mcts_sequence_perplexity_demo.py
@@ -0,0 +1,41 @@
+from chemgrams import *
+
+if __name__ == '__main__':
+
+    print("loading language model...")
+    lm = DeepSMILESLanguageModelUtils.get_lm("models/chembl_25_deepsmiles_lm_5gram_190330.pkl")
+
+    num_simulations = 1000
+    width = 3
+    text_length = 25
+    start_state = ["<M>"]
+
+    def eval_function(text):
+        generated = ''.join(text)
+        try:
+            decoded = DeepSMILESLanguageModelUtils.decode(generated)
+            DeepSMILESLanguageModelUtils.sanitize(decoded)
+        except Exception:
+            return 0
+        extracted = DeepSMILESLanguageModelUtils.extract(generated)
+        tokenized = DeepSMILESTokenizer(extracted)
+        len_reward = len(tokenized.get_tokens()) / (text_length - 1)  # provide more reward for longer text sequences
+        preplexity = lm.perplexity(text)
+        perplexity_reward = preplexity / (1 + preplexity)
+        return (perplexity_reward*0.5) + (len_reward*0.5)
+
+    mcts = LanguageModelMCTS(lm, width, text_length, eval_function)
+    state = start_state
+
+    print("beginning search...")
+    mcts.search(state, num_simulations)
+
+    best = mcts.get_best_sequence()
+
+    generated_text = ''.join(best[0])
+    print("generated text: %s (score: %s, perplexity: %s)" % (generated_text, str(best[1]), lm.perplexity(generated_text)))
+
+    decoded = DeepSMILESLanguageModelUtils.decode(generated_text)
+    smiles = DeepSMILESLanguageModelUtils.sanitize(decoded)
+
+    print("SMILES: %s" % smiles)
