bd3d9e6b16f154eefee81d89768d1d3eb51f47b2
Luis Antunes <lantunes@gmail.com>
2019-04-14 20:32:55 -0400

    fixing import

--- chemgrams/sascorer/sascorer.py
+++ chemgrams/sascorer/sascorer.py
@@ -19,7 +19,7 @@ from __future__ import print_function
 
 from rdkit import Chem
 from rdkit.Chem import rdMolDescriptors
-from rdkit.six.moves import cPickle
+import pickle as cPickle
 from rdkit.six import iteritems
 
 import math
